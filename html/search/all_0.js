cbff58728d4d3a359e06399b2777cb7e61ddf299
JuanRPDev <152338120+JuanRPDev@users.noreply.github.com>
2023-12-03 17:41:00 -0500

    Initial commit

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['authors_3a_0',['Authors:',['../index.html#integr_sec',1,'']]]
+];
---
+++ html/search/all_7.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['pablo_20rivera_20portilla_20y_20jhonny_20fuentes_0',['Juan Pablo Rivera Portilla y Jhonny Fuentes',['../index.html#inte1',1,'']]],
+  ['piggybank_1',['PiggyBank',['../index.html',1,'']]],
+  ['pkgservices_2',['pkgServices',['../namespacepkg_services.html',1,'']]],
+  ['pkgservices_3a_3apkginterfaces_3',['pkgInterfaces',['../namespacepkg_services_1_1pkg_interfaces.html',1,'pkgServices']]],
+  ['portilla_20y_20jhonny_20fuentes_4',['Juan Pablo Rivera Portilla y Jhonny Fuentes',['../index.html#inte1',1,'']]]
+];
